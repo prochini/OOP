9e20aa31a05c5748d4e4e637bcf0415e3a176005
prochini <42841117+prochini@users.noreply.github.com>
2019-08-11 23:04:49 +0800

    Create bind and this.js

---
+++ bind and this.js
@@ -0,0 +1,13 @@
+```js
+let dog = {
+  sound: 'woof',
+  talk: function(){
+    console.log(this.sound)
+  }
+}
+
+dog.talk()
+let talkFunction = dog.talk
+let boundFunction = talkFunction.bind(dog)
+boundFunction()
+```
